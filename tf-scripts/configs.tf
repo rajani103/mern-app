591f4ab174175916b597e17115ef26423a192780
Rajani Ekunde <rajaniekunde@gmail.com>
2024-10-24 20:12:20 +0000

    12th trigger: Deploy to gcp

--- tf-scripts/configs.tf
+++ tf-scripts/configs.tf
@@ -5,3 +5,9 @@ provider "google" {
 variable "project"{
     default = "second-core-431718-j2"
 }
+terraform {
+  backend "gcs" {
+    bucket = " terraform-state-backend12891y4o28r3hewi8892"
+    prefix     = "terraform/state"              
+  }
+}
